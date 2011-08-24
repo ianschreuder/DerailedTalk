641a4002545e55bf2957b204187d3d3f485d5fcf
Ian Schreuder <ian.schreuder@gmail.com>
2011-08-24 08:02:59 -0600

    initial commit

---
+++ Gemfile
@@ -0,0 +1,24 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.1.0.rc6'
+gem 'mysql2'
+gem 'jquery-rails'
+
+
+group :assets do
+  gem 'sass-rails', "  ~> 3.1.0.rc"
+  gem 'coffee-rails', "~> 3.1.0.rc"
+  gem 'uglifier'
+end
+
+group :test do
+  gem 'cover_me', '>= 1.0.0.rc6'
+  gem 'rspec-rails'
+  gem 'database_cleaner'
+  gem "factory_girl_rails", ">= 1.1"
+end
+
+group :development do
+  gem 'rspec-rails'
+  gem "jasmine"
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Derailed::Application.config.secret_token = 'f93825ebdca0993049ef016e12450e581943d74518986595a07b4c3de4886da6ccc8dbd71157b2641b54c5b247ec4351e2cc6b57c8e970e21c0209758c63e4ff'
